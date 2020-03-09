cb3e06bdc5c95794ecfc48f85cace9058432ea76
Miguel Rico <miguel.rico@crol.mx>
2020-03-08 23:12:37 -0700

    Updates

---
+++ precache-manifest.d7930c57ce88e9d95845e034495852e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fad1fb2ed594e42cdc1358111779f76",
+    "url": "/IrakuX/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d23867e1cda3d65bf576",
+    "url": "/IrakuX/monsters-rolodex/static/css/main.b96320aa.chunk.css"
+  },
+  {
+    "revision": "5972ab9a24d87de2b217",
+    "url": "/IrakuX/monsters-rolodex/static/js/2.40aee04e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/IrakuX/monsters-rolodex/static/js/2.40aee04e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d23867e1cda3d65bf576",
+    "url": "/IrakuX/monsters-rolodex/static/js/main.3585552d.chunk.js"
+  },
+  {
+    "revision": "7e2313c50327c3859b62",
+    "url": "/IrakuX/monsters-rolodex/static/js/runtime-main.c4b515b7.js"
+  }
+]);
